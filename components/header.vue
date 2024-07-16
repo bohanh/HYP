ac257e1509f0b65dd6d05070f4505f7396ec4438
Bohan Ha <bohan.ha@insa-rennes.fr>
2024-07-16 20:12:48 +0200

    fix error (correct path)

---
+++ components/header.vue
@@ -0,0 +1,109 @@
+<script setup lang="ts">
+import {useRoute} from 'vue-router';
+
+const landmarks = ["About us", "People", "Projects", "Services", "All activities", "Contact us"];
+
+const generateUrl = (name: string) => {
+  return '/' + name.toLowerCase().split(" ")[0];
+};
+
+const route = useRoute();
+const currentPath = route.path;
+
+const isLandmarkActive = (name: string) => {
+  return generateUrl(name) === currentPath;
+};
+</script>
+
+<template>
+  <div class="container-header">
+    <NuxtLink to="/" id="logo-link" class="landmark" tabindex="0">
+      <img
+          id="logo"
+          src="/HYP/contents/logo.svg"
+          alt="Logo"
+      />
+    </NuxtLink>
+    <div id="landmarks">
+      <NuxtLink
+          v-for="l in landmarks"
+          :key="l"
+          :to="generateUrl(l)"
+          class="landmark"
+          :class="{ 'active': isLandmarkActive(l) }"
+          tabindex="0"
+      >
+        {{ l }}
+      </NuxtLink>
+      <NuxtLink id="donation" to="/donate" class="donate-button" tabindex="0">
+        DONATE
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container-header {
+  width: 100vw;
+  height: 10vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  overflow: hidden;
+  box-sizing: border-box;
+  padding: 5px;
+  background-color: var(--header-color);
+  font-family: Futura;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1000;
+}
+
+#logo {
+  height: 40px;
+  margin: 10px;
+}
+
+#landmarks {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-inline: 20px;
+  gap: 20px;
+  font-size: 15pt;
+  font-weight: bold;
+}
+
+.landmark {
+  font-size: 100%;
+  color: black;
+  text-decoration: none;
+  transition: color 0.3s;
+  cursor: pointer;
+}
+
+.landmark:hover {
+  color: white;
+}
+
+.active {
+  color: white;
+}
+
+.donate-button {
+  font-size: 100%;
+  color: white;
+  background-color: var(--header-button-color);
+  border-radius: 10px;
+  text-decoration: none;
+  padding: 10px;
+  transition: background-color 0.3s, color 0.3s;
+  cursor: pointer;
+}
+
+.donate-button:hover {
+  background-color: white;
+  color: var(--header-button-color);
+}
+</style>
---
+++ pages/people/index.vue
@@ -0,0 +1,146 @@
+<script setup lang="ts">
+import {breadcrumbs} from "~/composables/breadcrumbs";
+import {Person} from "~/model/Person";
+import {assignPeople} from "~/utils";
+
+const crumbs = breadcrumbs();
+crumbs.value = ["/people"];
+
+let {data: data_people} = await useFetch("/api/people");
+const people: Person[] = assignPeople(JSON.parse(data_people.value!.people));
+</script>
+
+<template>
+  <div class="container">
+    <div class="header">THE TEAM</div>
+    <div class="title">
+      Our dedicated team is here for you.
+      Here you can see their work.
+    </div>
+    <div v-if="people.length === 0" class="person-card">No people found</div>
+    <div v-else class="person-cards">
+      <div v-for="person in people" class="person-card-container" >
+        <NuxtLink class="person-card" :to="'/people/' + person.id" tabindex="0">
+          <img
+              class="person-thumb"
+              :src="'/HYP/contents/people/' + person.id + '.jpg'"
+              :alt="'Photo of ' + person.name"
+          >
+          <span class="person-name">{{ person.name }}</span>
+          <span class="person-description">{{ person.description.substring(0, 120) + "..." }}</span>
+        </NuxtLink>
+        <!--          <router-link :to="'/cv/' + person.id" class="see-cv">See CV</router-link>-->
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding-block: 50px;
+  font-family: 'Futura', sans-serif;
+}
+
+.header {
+  font-size: 2rem;
+  margin-bottom: 10px;
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+.title {
+  font-size: 1.5rem;
+  width: 80%;
+  margin-bottom: 50px;
+  color: #34495e;
+}
+
+.person-cards {
+  width: 80%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 25px;
+}
+
+.person-card-container {
+  width: 23%;
+  min-width: 300px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 20px;
+  text-decoration: none;
+}
+
+.person-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  aspect-ratio: 2/3;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  position: relative;
+  text-decoration: none;
+  border-radius: 20px;
+  color: transparent;
+  background-color: transparent;
+  font-family: inherit;
+  font-size: inherit;
+  font-weight: inherit;
+  border: none;
+  padding: 0;
+  gap: 10px;
+}
+
+.person-thumb {
+  aspect-ratio: 1;
+  width: 100%;
+  object-fit: cover;
+}
+
+.person-name {
+  color: black;
+  font-size: 120%;
+  font-weight: bold;
+  text-decoration: none;
+  padding-inline: 5%;
+  text-overflow: ellipsis;
+}
+
+.person-description {
+  display: block;
+  color: black;
+  font-weight: normal;
+  text-decoration: none;
+  padding-inline: 5%;
+}
+
+.see-cv {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: calc(100% - 20px);
+  background-color: #ffffff;
+  color: #8e44ad;
+  padding: 6px 0;
+  border: 2px solid #8e44ad;
+  text-decoration: none;
+  transition: background-color 0.3s, color 0.3s, border-color 0.3s;
+  cursor: pointer;
+}
+
+.see-cv:hover {
+  background-color: #8e44ad;
+  color: #ffffff;
+  border-color: #8e44ad;
+}
+
+</style>
+
